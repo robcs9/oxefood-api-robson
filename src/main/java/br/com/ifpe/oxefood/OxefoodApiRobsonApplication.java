eb99149d836b8843e99362fbc70970e4ad6d7494
robcs9 <rcs25@discente.ifpe.edu.br>
2023-08-25 17:01:45 -0300

    Novos arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiRobsonApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiRobsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiRobsonApplication.class, args);
+	}
+
+}
